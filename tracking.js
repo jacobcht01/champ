f9a4cad2b89b99f5e196c9969a340b3e49bae221
jacobcht01 <leahmcdonal12@gmail.com>
2025-02-18 20:46:23 +0000

    Day 1 Set up CT-2025

--- tracking.js
+++ tracking.js
@@ -5,7 +5,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TZ7CF89V');`;
+    })(window,document,'script','dataLayer','GTM-NTRJ5KHX');`;
     document.head.appendChild(gtmScript);
 })();
 
@@ -29,7 +29,7 @@
 // 2️⃣ Insert Google Tag Manager noscript inside <body>
 document.addEventListener("DOMContentLoaded", function() {
     var gtmNoScript = document.createElement("noscript");
-    gtmNoScript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZ7CF89V"
+    gtmNoScript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTRJ5KHX"
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
     document.body.insertBefore(gtmNoScript, document.body.firstChild);
 });
